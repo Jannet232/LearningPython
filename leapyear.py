6d9f1c1a25d0264150e6f2bc0be2aac0550d0f9b
Jannet <hereby.jannetfern@gmail.com>
2022-08-14 17:03:22 +1000

    Second version of leapyear program: corrected code to use different logic for century years

--- leapyear.py
+++ leapyear.py
@@ -1,14 +1,6 @@
 #This program will determine if a year input by the user is a Leap Year or Not
 
 #Function to determine if year is divisible by 4 (remainder to be 0).
-# If remainder is 0, year is a Leap Year, so return True.
-# If remainder is not 0, year is not a leap Year, so return False.
-def divby4(a):
-    remainder = a%4
-    if remainder == 0:
-        return True
-    else:
-        return False
 
 #STEP 1: Inform user about the functionality of the program
 print('DETERMINE IF A YEAR IS A LEAP YEAR OR NOT')
@@ -22,20 +14,27 @@ while i == 'Y' or i == 'y':
     #STEP 4: Accept year as input from user and convert to int type
     year = int(input('Enter a Year : '))
 
-    #STEP 5: Check if number input is a year with 4 digits
-    length = len(str(year))
-    if length != 4:
-        year = int(input('Invalid Year. Enter a 4 digit Year: '))
-
     #STEP 5: Inform user that functionality is checking if year is leap year or not
     print(f'Checking if {year} is a leap year.')
 
-    #STEP 6: Call function to determine if the year is a leap year or not.
-    #Print result for user
-    if divby4(year):
-       print(f'{year} is a leap year as it is divisible by 4')
+    #STEP 6: Check if year is a century year by dividing by 100
+    centremainder = year % 100
+    #STEP 7: If year is a century year then if year is divisible by 400 it is a leap year
+    #If year is not divisible by 400 then it is not a leap year
+    if centremainder == 0:
+        remainder = year % 400
+        if remainder == 0:
+            print(f'{year} is a leap year as its divisible by 400.')
+        else:
+            print(f'{year} is not a leap year as its divisible by 100 but not by 400.')
+    #STEP 8: If year is not a century year then if year is divisible by 4 it is a leap
+    #If year is not divisible by 4 then it is not a leap year
     else:
-       print(f'{year} is not a leap year as it is not divisible by 4')
+        remainder = year % 4
+        if remainder == 0:
+            print(f'{year} is a leap year as its divisible by 4 but not by 100 or 400.')
+        else:
+            print(f'{year} is not a leap as its not divisible by 4 or 400. ')
 
-    #STEP 7: Ask user if they want to continue using the functionality for other years
+    #STEP 9: Ask user if they want to continue using the functionality for other years
     i = input(print('Do you want to use the functionality with another year (Y or N): '))
